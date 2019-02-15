88d4fe240a8394ef1dba61e0684977503ff6adcc
vijayst <vijayst@gmail.com>
2019-02-15 10:41:04 +0530

    services page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import SEO from '../components/seo';
-import Home from '../home/index';
+import styles from './index.module.css';
 
 const IndexPage = () => (
     <div>
         <SEO title="Vijay Consulting Services" keywords={[`gatsby`, `application`, `react`]} />
-        <Home />
+        <div className={styles.home}>
+            <div className={styles.center}>
+                <div className={styles.company}>
+                    Vijay Consulting Services
+                </div>
+                <p className={styles.offering}>We offer custom application development using React framework</p>
+                <button className={styles.button}><Link to="/services">View our services</Link></button>
+            </div>
+        </div>
     </div>
 );
 
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import styles from './index.module.css';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
+import styles from './services.module.css';
 
-export default function Home() {
+export default function Services() {
     return (
-        <div className={styles.home}>
+        <Layout>
             <div>
-                <div className={styles.company}>
-                    Vijay Consulting
-                </div>
+                <h1>Services</h1>
+                <p>View our service offerings:</p>
                 <ul className={styles.services}>
                     <li><Link to="/services/react-app">React apps</Link></li>
                     <li><Link to="/services/react-native-app">React Native apps</Link></li>
@@ -16,6 +16,6 @@ export default function Home() {
                     <li><Link to="/services/gatsby-app">Gatsby apps</Link></li>
                 </ul>
             </div>
-        </div>
+        </Layout>
     )
 }
