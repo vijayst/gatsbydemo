b0f810437961341bc54b1518843e4d5842e954f2
vijayst <vijayst@gmail.com>
2019-02-15 07:48:08 +0530

    home page

--- src/home/index.js
+++ src/home/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import styles from './index.module.css';
+import { Link } from 'gatsby';
 
 export default function Home() {
     return (
         <div className={styles.home}>
-            Hello world
+            <div>
+                <div className={styles.company}>
+                    Vijay Consulting
+                </div>
+                <ul className={styles.services}>
+                    <li><Link to="/services/react-apps">React apps</Link></li>
+                    <li><Link to="/services/react-apps">React Native apps</Link></li>
+                    <li><Link to="/services/react-apps">Next apps</Link></li>
+                    <li><Link to="/services/react-apps">Gatsby apps</Link></li>
+                </ul>
+            </div>
         </div>
     )
 }
